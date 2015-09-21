c8b6206e3f2576c27891ef40f526e78107f4ee84
m-arnold <michaelaarnold@gmail.com>
2015-09-20 20:54:14 -0700

    fix deploy and circleCI scripts

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,16 +1,14 @@
  #!/bin/bash
 
-EC2KEY = "/Users/michaelarnold/Documents/tersr-key.pem"
+EC2KEY="/Users/michaelarnold/Documents/tersr-key.pem"
 
 # echo CLEANING!
 # gulp clean
 # echo BUILDING!
 # gulp build
 echo COPYING
-scp -r -i EC2KEY server ec2-user@52.27.205.191:tersr
-scp -i EC2KEY package.json ec2-user@52.27.205.191:tersr
+scp -i $EC2KEY -r server/* ec2-user@52.27.205.191:tersr
+scp -i $EC2KEY package.json ec2-user@52.27.205.191:tersr
 echo TUNNELING!
-ssh -i EC2KEY ec2-user@52.27.205.191 <<'ENDSSH'
-export NODE_ENV=production
-ENDSSH
+ssh -i $EC2KEY ec2-user@52.27.205.191 'cd; cd tersr; export NODE_ENV=production; node app.js; exit'
 echo COMPLETED
