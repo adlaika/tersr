a6261bf9385ce07ba92d0e39c2bbb740d717a715
m-arnold <michaelaarnold@gmail.com>
2015-09-22 22:38:45 -0700

    add error handling and trimSlashes, implement isMyRoute check

--- server/controllers/linksController.js
+++ server/controllers/linksController.js
@@ -1,17 +1,29 @@
 var Link = require('../models/linksModel')
 
-var addNewLink = function(req, res) {
-    Link.addNewLink(req.body.url, function(err, message, short, url) {
-        res.render('index', {link: {short: req.header('host') + '/' + short, url: url}})
+var addNewLink = function(req, res, next) {
+    Link.addNewLink(req.body.url, function (err, message, short, url) {
+        err ? next(err) : res.render('index', {link: {short: req.header('host') + '/' + short, url: url}})
     })
 }
 
-var goToUrl = function(req, res) {
-    var short = req.path.substring(3)
-    Link.getLink(short, function(err, linkObj) {
-        console.log("linkObj: ", linkObj)
-        res.redirect(linkObj.url)
-    })
+var goToUrl = function(req, res, next) {
+    // only go to short url if path is not one of my routes, else hand request to next matching handler
+    if (!res.locals.isMyRoute) {
+        var short = trimSlashes(req.path)
+        Link.getLink(short, function (err, linkObj) {
+            err ? next(err) : res.redirect(linkObj.url)
+        })
+    } else next()
+}
+
+function trimSlashes (path) {
+    if (path[path.length - 1] === '/') {
+        path = path.substring(0, path.length - 1)
+    }
+    if (path[0] === '/') {
+        path = path.substring(1)
+    }
+    return path
 }
 
 module.exports = {
