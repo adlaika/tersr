2aaeec301fd5459e74704dd7fab3f7a200be234f
m-arnold <michaelaarnold@gmail.com>
2015-09-23 22:17:39 -0700

    finish consolidating tests, write more API tests

--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,20 @@
 var should = require('should')
 var redis = require('redis')
+var hippie = require('hippie')
 
 var Link = require('../server/models/linksModel')
+var util = require('../server/helpers/util.js')
+var app = require('../server/app.js')
+
 var db
 
-describe('Database Interaction Tests', function() {
-    describe('Model - Link', function() {
+describe('Database Tests', function() {
+    describe('Link Model', function() {
         before(function(done) {
+            // create new db client connect to ensure no interference from main app
             db = redis.createClient()
             db.on('connect', function() {
-                console.log('redis connected')
+                console.log('       redis connected')
             })
             db.flushdb()
             db.set('foo', 'bar')
@@ -78,7 +83,64 @@ describe('Database Interaction Tests', function() {
                 done()
             })
         })
-
-
     })
 })
+
+describe('Utility Tests', function() {
+    describe('trimSlashes', function() {
+        it('should throw an error if argument is not a string', function() {
+            (function() { util.trimSlashes(null) }).should.throw();
+            (function() { util.trimSlashes(true) }).should.throw();
+            (function() { util.trimSlashes(2) }).should.throw();
+        })
+        it('should return the same path if it contains no slashes', function() {
+            util.trimSlashes('foo').should.equal('foo')
+        })
+        it('should return the path minus trailing and/or leading slashes', function() {
+            util.trimSlashes('/foo/').should.equal('foo')
+            util.trimSlashes('/foo').should.equal('foo')
+            util.trimSlashes('foo/').should.equal('foo')
+        })
+        it('should not remove interstitial slashes', function() {
+            util.trimSlashes('foo/bar').should.equal('foo/bar')
+            util.trimSlashes('/foo/bar/').should.equal('foo/bar')
+            util.trimSlashes('/foo/bar').should.equal('foo/bar')
+            util.trimSlashes('foo/bar/').should.equal('foo/bar')
+        })
+    })
+})
+
+describe('API Tests', function() {
+    it('GET to // should return 404', function(done) {
+        api()
+            .get('//')
+            .expectStatus(404)
+            .end(function(err, res, body) {
+                if (err) throw err
+                done()
+            })
+    })
+    it('GET to non-extant short link should 404', function(done) {
+        api()
+            .get('/dfsdlfjs3353lgflj33')
+            .expectStatus(404)
+            .end(function(err, res, body) {
+                if (err) throw err
+                done()
+            })
+    })
+    it('PUT to / should add a link', function() {
+        api()
+            .put('/')
+            .expectStatus(201)
+            .end(function(err, res, body) {
+                if (err) throw err
+                done()
+            })
+    })
+})
+
+function api() {
+    return hippie()
+        .base(app.HOST)
+}
