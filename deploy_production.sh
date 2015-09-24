075540d9b4c48eda49c0cdc0a26190d32ce5bde6
m-arnold <michaelaarnold@gmail.com>
2015-09-24 09:25:40 -0700

    update deploy script to use nodemon instead of node

--- deploy_production.sh
+++ deploy_production.sh
@@ -13,5 +13,5 @@ scp -i $EC2KEY -r server/* ec2-user@52.27.205.191:tersr
 scp -i $EC2KEY package.json ec2-user@52.27.205.191:tersr
 echo TUNNELING!
 # TODO: figure out how to send ctrl-a d keystroke to remote machine so that this machine doesn't get stuck in node's output
-ssh -t -i $EC2KEY ec2-user@52.27.205.191 'cd; cd tersr; killall node; export NODE_ENV=production; screen node app.js'
+ssh -t -i $EC2KEY ec2-user@52.27.205.191 'cd; cd tersr; killall node; export NODE_ENV=production; screen nodemon app.js'
 echo COMPLETED
