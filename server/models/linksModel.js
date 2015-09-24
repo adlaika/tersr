77813fbdadc8e93d982738526004a829c26f6def
m-arnold <michaelaarnold@gmail.com>
2015-09-24 10:16:56 -0700

    add type enforcement to linkModel methods

--- server/models/linksModel.js
+++ server/models/linksModel.js
@@ -1,18 +1,22 @@
 var db = require('../app').db
 
 var set = function(key, value, cb) {
+    if (typeof cb !== 'function') throw new TypeError(cb + ' is not a function!')
     db.set(key, value, function(err, message) {
         cb(message)
     })
 }
 
 var getUniqueId = function(cb) {
+    if (typeof cb !== 'function') throw new TypeError(cb + ' is not a function!')
     db.incr('linkCounter', function(err, reply) {
         cb(err, reply)
     })
 }
 
 var addNewLink = function(url, cb) {
+    if (typeof url !== 'string') throw new TypeError(cb + ' is not a string (url)!')
+    if (typeof cb !== 'function') throw new TypeError(cb + ' is not a function!')
     getUniqueId(function(err, id) {
         if (id) {
             //set key as "link:" + id to differentiate from other types, like "users"
@@ -31,6 +35,7 @@ var addNewLink = function(url, cb) {
 }
 
 var getLink = function(short, cb) {
+    if (typeof cb !== 'function') throw new TypeError(cb + ' is not a function!')
     //convert base36 back to base10 so we can retrieve record at id
     var linkId = parseInt(short, 36)
     var linkKey = 'link:' + linkId
